df1fe506979ca3d9a2fd062c9321e1ea55354d1b
syam051 <gokarlasyamsundar51@gmail.com>
2024-12-20 22:12:30 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,15 +1,15 @@
+-- Create Suppliers Table
+CREATE TABLE Suppliers (
+    SupplierID NUMBER PRIMARY KEY,
+    SupplierName VARCHAR2(100) NOT NULL,
+    Contact VARCHAR2(50)
+);
+
 -- Create Products Table
 CREATE TABLE Products (
     ProductID NUMBER  PRIMARY KEY,
     ProductName VARCHAR2(100) NOT NULL,
     Quantity NUMBER NOT NULL,
     Price NUMBER(10, 2) NOT NULL,
-    SupplierID NUMBER NOT NULL REFERENCES Suppliers(SupplierID)
-);
-
--- Create Suppliers Table
-CREATE TABLE Suppliers (
-    SupplierID NUMBER PRIMARY KEY,
-    SupplierName VARCHAR2(100) NOT NULL,
-    Contact VARCHAR2(50)
+    SupplierID NUMBER REFERENCES Suppliers(SupplierID)
 );
