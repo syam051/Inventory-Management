afce43d3f9408e8869f2f1ef458e132a0ec9aa7e
syam051 <gokarlasyamsundar51@gmail.com>
2024-12-20 22:11:38 +0530

    Update Inser_data.sql

--- Inser_data.sql
+++ Inser_data.sql
@@ -1,10 +1,8 @@
 -- Insert Data into Suppliers Table
-INSERT INTO Suppliers (SupplierName, Contact) VALUES 
-('Tech Supplies Ltd.', 'contact@techsupplies.com'),
-('Office Equipment Inc.', 'info@officeequipment.com');
+INSERT INTO Suppliers VALUES (1,'Microsoft', 'microsoft@techsupplies.com');
+INSERT INTO Suppliers VALUES (2,'Qtel Comtech Limited', 'QCL@officeequipment.com');
 
 -- Insert Data into Products Table
-INSERT INTO Products (ProductName, Quantity, Price, SupplierID) VALUES 
-('Laptop', 50, 55000.00, 1),
-('Keyboard', 150, 1500.00, 2),
-('Mouse', 200, 500.00, 1);
+INSERT INTO Products VALUES (100,'Laptop', 50, 55000.00, 1);
+INSERT INTO Products VALUES (105,'Keyboard', 150, 1500.00, 2);
+INSERT INTO Products VALUES (110,'Mouse', 200, 500.00, 1);
